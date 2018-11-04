1d372228e15ba756531ff79b04f97da845ff2f0b
brian91292 <8050523+brian91292@users.noreply.github.com>
2018-11-04 13:27:59 -0600

    Initial release

---
+++ DebrisRemover/GameHooks.cs
@@ -0,0 +1,63 @@
+﻿using PlayHooky;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using UnityEngine;
+using System.Diagnostics;
+
+namespace DebrisRemover
+{
+
+    public class NoteDebrisSpawnerHooks
+    {
+        public static void SpawnDebris(NoteDebrisSpawner t, NoteCutInfo noteCutInfo, NoteController noteController)
+        {
+        }
+    }
+
+    public class GameHooks : MonoBehaviour {
+        private HookManager hookManager;
+        private Dictionary<string, MethodInfo> hooks;
+
+        private void Awake() {
+            UnityEngine.Object.DontDestroyOnLoad((UnityEngine.Object)((UnityEngine.Component)this).gameObject);
+            this.hookManager = new HookManager();
+            this.hooks = new Dictionary<string, MethodInfo>();
+
+            this.Hook("DebrisSpawner", typeof(NoteDebrisSpawner).GetMethod("SpawnDebris"), typeof(NoteDebrisSpawnerHooks).GetMethod("SpawnDebris"));
+        }
+
+        private void OnDestroy() {
+            foreach (string key in this.hooks.Keys)
+                this.UnHook(key);
+        }
+
+        private bool Hook(string key, MethodInfo target, MethodInfo hook) {
+            if (this.hooks.ContainsKey(key))
+                return false;
+            try {
+                this.hooks.Add(key, target);
+                this.hookManager.Hook(target, hook);
+                Plugin.Log($"{key} hooked!");
+                return true;
+            }
+            catch (Win32Exception ex) {
+                Plugin.Log($"Unrecoverable Windows API error: {(object)ex}");
+                return false;
+            }
+            catch (Exception ex) {
+                Plugin.Log($"Unable to hook method, : {(object)ex}");
+                return false;
+            }
+        }
+
+        private bool UnHook(string key) {
+            MethodInfo original;
+            if (!this.hooks.TryGetValue(key, out original))
+                return false;
+            this.hookManager.Unhook(original);
+            return true;
+        }
+    }
+}
---
+++ DebrisRemover/Plugin.cs
@@ -0,0 +1,46 @@
+﻿using IllusionPlugin;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DebrisRemover
+{
+    public class Plugin : IPlugin
+    {
+        public string Name => "DebrisRemover";
+        public string Version => "1.0.0";
+
+        private GameHooks _gameHooks;
+
+        public void OnApplicationStart()
+        {
+            _gameHooks = new GameObject().AddComponent<GameHooks>();
+        }
+
+        public void OnApplicationQuit()
+        {
+        }
+
+        public void OnLevelWasLoaded(int level)
+        {
+
+        }
+
+        public void OnLevelWasInitialized(int level)
+        {
+        }
+
+        public void OnUpdate()
+        {
+        }
+
+        public void OnFixedUpdate()
+        {
+        }
+
+        public static void Log(string msg)
+        {
+            Console.WriteLine($"[DebrisRemover] {msg}");
+        }
+    }
+}
