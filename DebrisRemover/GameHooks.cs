f6747081f386ed5f9c76171cc0fe75367c111eeb
brian91292 <8050523+brian91292@users.noreply.github.com>
2018-11-04 14:10:20 -0600

    Add gameplay options toggle

--- DebrisRemover/GameHooks.cs
+++ DebrisRemover/GameHooks.cs
@@ -16,43 +16,59 @@ namespace DebrisRemover
         }
     }
 
-    public class GameHooks : MonoBehaviour {
+    public class GameHooks : MonoBehaviour
+    {
         private HookManager hookManager;
         private Dictionary<string, MethodInfo> hooks;
 
-        private void Awake() {
+        private void Awake()
+        {
             UnityEngine.Object.DontDestroyOnLoad((UnityEngine.Object)((UnityEngine.Component)this).gameObject);
             this.hookManager = new HookManager();
             this.hooks = new Dictionary<string, MethodInfo>();
+        }
 
+        private void OnDestroy()
+        {
+            RemoveHooks();
+        }
+
+        public void ApplyHooks()
+        {
             this.Hook("DebrisSpawner", typeof(NoteDebrisSpawner).GetMethod("SpawnDebris"), typeof(NoteDebrisSpawnerHooks).GetMethod("SpawnDebris"));
         }
 
-        private void OnDestroy() {
+        public void RemoveHooks()
+        {
             foreach (string key in this.hooks.Keys)
                 this.UnHook(key);
         }
 
-        private bool Hook(string key, MethodInfo target, MethodInfo hook) {
+        private bool Hook(string key, MethodInfo target, MethodInfo hook)
+        {
             if (this.hooks.ContainsKey(key))
                 return false;
-            try {
+            try
+            {
                 this.hooks.Add(key, target);
                 this.hookManager.Hook(target, hook);
                 Plugin.Log($"{key} hooked!");
                 return true;
             }
-            catch (Win32Exception ex) {
+            catch (Win32Exception ex)
+            {
                 Plugin.Log($"Unrecoverable Windows API error: {(object)ex}");
                 return false;
             }
-            catch (Exception ex) {
+            catch (Exception ex)
+            {
                 Plugin.Log($"Unable to hook method, : {(object)ex}");
                 return false;
             }
         }
 
-        private bool UnHook(string key) {
+        private bool UnHook(string key)
+        {
             MethodInfo original;
             if (!this.hooks.TryGetValue(key, out original))
                 return false;
--- DebrisRemover/Plugin.cs
+++ DebrisRemover/Plugin.cs
@@ -11,10 +11,48 @@ namespace DebrisRemover
         public string Version => "1.0.0";
 
         private GameHooks _gameHooks;
+        public static bool Enabled = true;
 
         public void OnApplicationStart()
         {
+            SceneManager.activeSceneChanged += SceneManagerOnActiveSceneChanged;
+            SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+
             _gameHooks = new GameObject().AddComponent<GameHooks>();
+        }
+
+        private void ToggleDebrisRemover(bool enabled)
+        {
+            if (enabled) _gameHooks.ApplyHooks();
+            else _gameHooks.RemoveHooks();
+        }
+
+        private void SceneManagerOnActiveSceneChanged(Scene arg0, Scene arg1)
+        {
+            if (arg1.name == "Menu")
+            {
+                var tmpEnabled = ModPrefs.GetBool(Name, "Enabled", Enabled);
+                if (tmpEnabled != Enabled)
+                {
+                    ToggleDebrisRemover(tmpEnabled);
+                    Enabled = tmpEnabled;
+                }
+
+                var toggle = GameOptionsUI.CreateToggleOption("Debris Remover");
+                toggle.GetValue = Enabled;
+                toggle.OnToggle += (b) =>
+                {
+                    Enabled = b;
+                    Plugin.Log($"{(b ? "Enabled" : "Disabled")}");
+                    ModPrefs.SetBool(Name, "Enabled", Enabled);
+
+                    ToggleDebrisRemover(b);
+                };
+            }
+        }
+
+        private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
+        {
         }
 
         public void OnApplicationQuit()
